50c72c94fe08aa0e30aa03d77347ae0fd85f1c78
Francisco Presencia <franciscop@users.noreply.github.com>
2019-04-06 21:12:07 -0700

    Added flag for the language
    Allow to pass the language from the command

--- cli.js
+++ cli.js
@@ -7,6 +7,8 @@ const cli = meow(`
 	Examples
 	  $ pokemon
 	  Snorlax
+	  $ pokemon --lang ja
+	  マクノシタ
 	  $ pokemon --all
 	  Bulbasaur
 	  Ivysaur
@@ -14,6 +16,8 @@ const cli = meow(`
 
 	Options
 	  --all  Get all the names instead of a random name
+	  --lang Set the language for the pokemon. Defaults to 'en'
 `);
 
-console.log(cli.flags.all ? pokemon.all().join('\n') : pokemon.random());
+const lang = cli.flags.lang || 'en';
+console.log(cli.flags.all ? pokemon.all(lang).join('\n') : pokemon.random(lang));
